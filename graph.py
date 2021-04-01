41619c65a012fc853e37234ac56dd34d36a49006
marcihel05 <helena.marcius05@gmail.com>
2021-04-01 13:46:08 +0200

    small changes

--- graph.py
+++ graph.py
@@ -2,27 +2,28 @@ import networkx as nx
 import numpy as np
 import random
 import math
+import matplotlib.pyplot as plt
 
-class Tour():
+class Map():
     def __init__(self, n):
         self.num_of_cities = n
-        self.xy = np.zeros((n,2))
+        self.xy = np.empty((n,2))
         for i in range(n):
-            self.xy[i] = random.randint(0, 500), random.randint(0,500)
-        self.distances = np.zeros((n,n))
-        self.calculate_distances()
+            self.xy[i] = random.randint(0, 100), random.randint(0,100)
+        self.distances = self.calculate_distances()
 
-
-    def calculate_distances():
+    def calculate_distances(self):
+        distances = np.empty((self.num_of_cities, self.num_of_cities))
         for i in range(self.num_of_cities):
             p = self.xy[i]
             for j in range(self.num_of_cities):
                 q = self.xy[j]
-                self.distances[i][j] = math.dist(p,q)
+                distances[i][j] = math.sqrt((p[0]-q[0])**2+(p[1]-q[1])**2)
+        return distances
     
-    def draw_graph():
+    #def draw_graph():
 
-    def draw_coordinate():
+    #def draw_coordinate():
 
 
         
