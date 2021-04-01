672d44e8b9393d0322dfbbb2b1f943a92cffb61f
marcihel05 <helena.marcius05@gmail.com>
2021-04-01 13:40:15 +0200

    main program

--- travel.py
+++ travel.py
@@ -5,23 +5,21 @@ import math
 
 def main():
     num_of_cities = 50
-    num_of_routes = 100
-    num_of_generations = 500
+    num_of_routes = 25
+    num_of_generations = 10000
     graph = Map(num_of_cities)
     population = Population(num_of_routes, num_of_cities, graph)
-    new_dist = old_dist = 0
-    #for i in range(num_of_generations):
-    i = 1
-    while True:
+    for i in range(num_of_generations):
         population.geneticAlgorithm()
-        print(i)
-        i+=1
-        new_dist = population.best_route.distance
-        print(new_dist)
-        if abs(new_dist-old_dist) < 10**(-5):
-            break
-        old_dist = new_dist
+        
+    
+   # print(population.best_route.path)
+    print(population.best_distance_in_generation[0])
+    for i in range(num_of_generations):
+        if i % 250 == 0:
+            print(population.best_distance_in_generation[i])
     print(population.best_route.distance)
+    
 
 main()
 
