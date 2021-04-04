6c8489bf2433d26544d95afe6cc32b1c7fd487c3
marcihel05 <helena.marcius05@gmail.com>
2021-04-04 22:12:19 +0200

    main modified

--- travel.py
+++ travel.py
@@ -6,21 +6,11 @@ from route import Route
 from graph import Map
 import math
 import time
+import numpy as np
+from settings import *
 
 
-WIN_WIDTH = 1200
-WIN_HEIGHT = 800
-
-NUM_OF_CITIES = 50
-NUM_OF_ROUTES = 25
-NUM_OF_GENERATIONS = 50000
-
-FPS = 10
-
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-YELLOW = (255, 255, 0)
-
+FPS = 30
 
 pygame.font.init()
 FONT = pygame.font.SysFont("monospace", 25)
@@ -31,10 +21,11 @@ def draw_window(win, population, i):
     population.draw_path(win)
     gen_text = FONT.render('Current generation: ' + str(i), 1, WHITE)
     dist_text = FONT.render('Shortest distance: ' + str(population.best_route.distance), 1, WHITE)
+    first_dist_text = FONT.render('Starting distance: ' + str(population.best_distance_in_generation[0]), 1, WHITE)
     win.blit(gen_text, (500, 10))
-    win.blit(dist_text, (500, 30))
+    win.blit(dist_text, (500, 60))
+    win.blit(first_dist_text, (500, 35))
     pygame.display.update()
-
 
 
 def main():
@@ -42,7 +33,7 @@ def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     pygame.display.set_caption('Travelling Salesman Problem')
     clock = pygame.time.Clock()
-    graph = Map(NUM_OF_CITIES)
+    graph = Map(NUM_OF_CITIES, 'load')
     population = Population(NUM_OF_ROUTES, NUM_OF_CITIES, graph)
     run = True
     i = 0
@@ -54,8 +45,14 @@ def main():
         population.geneticAlgorithm()
         i +=1
         draw_window(win, population, i)
+    while run:
+        draw_window(win, population, i)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
     print(population.best_distance_in_generation[0])
     print(population.best_route.distance)
+    population.save_path()
     
 
 main()
