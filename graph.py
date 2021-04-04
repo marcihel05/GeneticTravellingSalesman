21efe91a67b0e37984f7f37299f1e4e7a5ab79bc
marcihel05 <helena.marcius05@gmail.com>
2021-04-04 18:15:56 +0200

    diff graph options

--- graph.py
+++ graph.py
@@ -1,13 +1,17 @@
 import numpy as np
 import random
 import math
+from settings import *
 
 class Map():
-    def __init__(self, n):
+    def __init__(self, n, option = 'random'):
         self.num_of_cities = n
-        self.xy = np.empty((n,2))
-        for i in range(n):
-            self.xy[i] = random.randint(0, 100), random.randint(0,100)
+        if option == 'random':
+            self.xy = np.array([random.uniform(200, 600), random.uniform(200,600)] for i in range(n))
+        if option == 'circle':
+            self.xy = np.array([[math.cos(t), math.sin(t)]*250 + 400 for t in np.linspace(0, 2*math.pi, n)])
+        if option == 'load':
+            self.xy = self.load_from_file()
         self.distances = self.calculate_distances()
 
     def calculate_distances(self):
@@ -18,7 +22,19 @@ class Map():
                 q = self.xy[j]
                 distances[i][j] = math.sqrt((p[0]-q[0])**2+(p[1]-q[1])**2)
         return distances
+    
+    def load_from_file(self):
+        arr = np.loadtxt(FILE)
+        mn =  -min(arr[:,0])
+        for i in range(self.num_of_cities):
+            arr[i][0] += mn
+        for i in range(self.num_of_cities):
+            arr[i] += 55
+            arr[i] /= 7
+        print(arr)
+        return arr
 
+                #NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
 
 
 
